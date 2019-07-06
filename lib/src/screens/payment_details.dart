941a4441147d6cdf248fc9979bf52576813ab135
Basit Mir <iammirbasit@gmail.com>
2019-07-06 12:56:08 +0530

    save changes on payment page

--- lib/src/screens/payment_details.dart
+++ lib/src/screens/payment_details.dart
@@ -31,59 +31,71 @@ class Payment extends StatelessWidget {
               child: Container(
                 alignment: AlignmentDirectional.topCenter,
                 child: CircleAvatar(
-                  radius: 150.0,
+                  radius: 140.0,
                   backgroundImage: AssetImage('assets/payment.png'),
                   backgroundColor: Colors.transparent,
                 ),
               ),
             ),
             Expanded(
-              child: Container(
-                margin: EdgeInsets.symmetric(horizontal: 12.00),
-                width: MediaQuery.of(context).size.width,
-                decoration: const BoxDecoration(
-                  color: Colors.white,
-                  gradient: LinearGradient(
-                    begin: FractionalOffset(0.7, 0.8),
-                    end: FractionalOffset(0.4, 0.0),
-                    colors: <Color>[
-                      Color(0xFFFFFB74D),
-                      Colors.white,
+              child: ListView(
+                children: <Widget>[
+                  Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(12.00, 80.00, 12.00, 00.00),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: const BoxDecoration(
+                          color: Colors.white,
+                          gradient: LinearGradient(
+                            begin: FractionalOffset(0.7, 0.8),
+                            end: FractionalOffset(0.6, 0.0),
+                            colors: <Color>[
+                              Color(0xFFFFFB74D),
+                              Colors.white,
+                            ],
+                          ),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              height: 90.0,
+                            ),
+                            Text(
+                              'You will be charged ₹5.00 for every minute from the time of booking',
+                              style: TextStyle(
+                                  fontSize: 30.0,
+                                  color: Colors.orange,
+                                  fontWeight: FontWeight.bold),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                            Text(
+                              'Note: Online Payment services will be included soon...',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white70,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 20.0,),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        alignment: AlignmentDirectional.topCenter,
+                        child: CircleAvatar(
+                          radius: 70.0,
+                          backgroundImage: NetworkImage(
+                              'https://image.flaticon.com/icons/png/512/199/199558.png'),
+                          backgroundColor: Colors.transparent,
+                        ),
+                      ),
                     ],
                   ),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      child:
-                    CircleAvatar(
-                      radius: 80.0,
-                      backgroundImage: NetworkImage(
-                          'https://image.flaticon.com/icons/png/512/199/199558.png'),
-                      backgroundColor: Colors.transparent,
-                    ),
-                    ),
-                    SizedBox(height: 10.0,),
-                    Text(
-                      'You will be charged ₹5.00 for every minute from the time of booking',
-                      style: TextStyle(
-                          fontSize: 30.0,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                     SizedBox(height: 20.0,),
-
-                    Text(
-                      'Note: Online Payment services will be included soon...',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white70,
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                  ],
-                ),
+                ],
               ),
             ),
             // SizedBox(height: 50),
